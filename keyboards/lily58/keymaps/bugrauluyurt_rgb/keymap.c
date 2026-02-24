a678924115d618892921b95962a6c29630acf947
Bugra Uluyurt <bugra.u@gmail.com>
2026-02-23 23:32:09 -0500

    chore: updated ctrl position

--- keyboards/lily58/keymaps/bugrauluyurt_rgb/keymap.c
+++ keyboards/lily58/keymaps/bugrauluyurt_rgb/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LGUI,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,  KC_MUTE,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_LEFT, KC_RGHT
+                        KC_LCTL, KC_LALT, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_LEFT, KC_RGHT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, D3_K3,   D3_KM1, _______, _______, _______,                    _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_PIPE,
   _______, _______, _______,_______, _______, _______,                    KC_PGUP, _______, KC_LT,   KC_GT,   KC_UP,   _______,
   KC_LSFT, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT,
-                             KC_LGUI, KC_LALT, _______, KC_SPC,  KC_ENT,  _______, KC_HOME, KC_END
+                             KC_LCTL, KC_LALT, _______, KC_SPC,  KC_ENT,  _______, KC_HOME, KC_END
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -222,7 +222,7 @@ void matrix_scan_user(void) {
 void keyboard_post_init_user(void) {
     // Initialize RGB to static black
     rgblight_enable_noeeprom();
-    rgblight_sethsv_noeeprom(HSV_BLUE);
+    rgblight_sethsv_noeeprom(HSV_WHITE);
     rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
 }
 
